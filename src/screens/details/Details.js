557061ecc17b2c68818a6944355aefa96de315be
Deepa Murty Tekumalla <naradeepu@gmail.com>
2019-05-09 16:12:17 +0200

    Added poster, title and genres on details page

--- src/screens/details/Details.js
+++ src/screens/details/Details.js
@@ -1,30 +1,32 @@
 import React, { Component } from 'react';
 import Header from '../../common/header/Header';
 import moviesData from '../../common/movieData';
+import Typography from '@material-ui/core/Typography';
+import './Details.css';
 
 class Details extends Component {
 
 
 
-    constructor(){
-            super();
-            this.state = {
-                movie: {}
-            }
+    constructor() {
+        super();
+        this.state = {
+            movie: {}
+        }
 
     }
 
-    componentDidMount(){
+    componentWillMount() {
         let currentState = this.state;
-        currentState.movie = moviesData.filter((mov)=>{
+        currentState.movie = moviesData.filter((mov) => {
             return mov.id === this.props.movieId;
         })[0];
-        this.setState({currentState});
+        this.setState({ currentState });
 
 
     }
     render() {
-
+        let movie = this.state.movie;
 
         return (
             <div className="details">
@@ -33,8 +35,27 @@ class Details extends Component {
 
                 <div className="flex-containerDetails">
                     <div className="leftDetails">
+                        <img src={movie.poster_url} alt={movie.title} />
                     </div>
                     <div className="middleDetails">
+                        <div>
+                            <Typography variant="headline" component="h2">{movie.title}</Typography>
+                        </div>
+                        <div>
+                            <Typography><span className="bold">Genre: </span>{movie.genres.join(',')}</Typography>
+                        </div>
+                        <div>
+                            <Typography><span className="bold">Duration: </span>{movie.duration}</Typography>
+                        </div>
+                        <div>
+                            <Typography><span className="bold">Release Date:</span> {new Date(movie.release_date).toDateString()} </Typography>                    
+                        </div>
+                        <div>
+                            <Typography><span className="bold"> Rating:</span> {movie.critics_rating}  </Typography>
+                        </div>
+                        <div className="marginTop16">
+                            <Typography><span className="bold">Plot:</span> <a href={movie.wiki_url}>(Wiki Link)</a> {movie.storyline} </Typography>
+                        </div>
                     </div>
                     <div className="rightDetails">
                     </div>
@@ -48,4 +69,4 @@ class Details extends Component {
 
 }
 
-export default details;
+export default Details;
